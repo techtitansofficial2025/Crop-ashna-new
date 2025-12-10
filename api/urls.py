1ae3d04d16a0a541af53f0810b68c792ad7fa363
techtitansofficial2025 <techtitans.official2025@gmail.com>
2025-12-11 04:19:28 +0530

    Create urls.py

---
+++ api/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import PredictView, HealthView
+
+urlpatterns = [
+    path("predict/", PredictView.as_view(), name="predict"),
+    path("health/", HealthView.as_view(), name="health"),
+]
