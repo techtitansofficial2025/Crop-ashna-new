e505d2fe8bad03e3360186dd287d1e831313405a
techtitansofficial2025 <techtitans.official2025@gmail.com>
2025-12-11 13:12:08 +0530

    Update views.py

--- api/views.py
+++ api/views.py
@@ -6,6 +6,14 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.conf import settings
 from .serializers import PredictSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(["GET"])
+def ping(request):
+    # lightweight ping for uptime checks
+    return Response({"ping": "pong"})
+
 
 logger = logging.getLogger("api")
 
