792185b8ac2fedd8afaa3a4b36208c3b5b8a5060
techtitansofficial2025 <techtitans.official2025@gmail.com>
2025-12-11 14:34:23 +0530

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PredictView, HealthView
+from .views import PredictView, HealthView, ping, debug_file_list
 
 urlpatterns = [
     path("predict/", PredictView.as_view(), name="predict"),
     path("health/", HealthView.as_view(), name="health"),
     path("ping/", ping, name="ping"),
+    path("debug_files/", debug_file_list, name="debug_files"),
 ]
